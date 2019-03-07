b1a1f5b52771db65f3dff84f3d7af269f692adb4
unknown <User@INFBN-53.up.ac.za>
2019-03-07 12:18:23 +0200

    INF354-A5 part 1

---
+++ INF354-API/INF354-API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace INF354_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ INF354-API/INF354-API/Controllers/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace INF354_API.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        // GET api/<controller>
+        [System.Web.Http.Route("api/Employee/getAll")]
+        [System.Web.Http.HttpPost]
+        public IEnumerable<string> getAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
